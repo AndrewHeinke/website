df55fcd7226dde53765a713d6114286352877dd5
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-08-30 14:24:22 -0500

    feat: portfolio page complete

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,22 +1,68 @@
-import { Box, Container, Grid, Heading } from "@chakra-ui/react";
+import {
+  Text,
+  Container,
+  Grid,
+  Heading,
+  Link,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import ProjectCard from "components/ProjectCard";
 
 function Portfolio() {
   return (
     <Container maxW="container.xl" mt="12">
       <Heading mb="4">Portfolio</Heading>
-      <Grid templateColumns="repeat(auto-fit, minmax(240px, 1fr))" gap={6}>
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
-        <Box w="100%" h="200px" bg="blue.500" />
+      <Text fontSize="xl" mb="8">
+        Curious about my latest professional projects?{" "}
+        <Link
+          color={useColorModeValue("purple.600", "purple.300")}
+          lineHeight="1.5"
+          _hover={{
+            textDecoration: "underline",
+          }}
+          href="mailto:andrew.j.heinke@gmail.com"
+        >
+          Send me an email
+        </Link>
+        , let's chat.
+      </Text>
+      <Grid templateColumns="repeat(auto-fit, minmax(400px, 1fr))" gap={6}>
+        <ProjectCard
+          title="Elderly"
+          description="A web application to connect elderly people with local volunteers."
+          image="/images/elderly.jpg"
+          link="http://pacific-hamlet-23272.herokuapp.com/"
+        />
+        <ProjectCard
+          title="LineUp"
+          description="See how long the line is before you vote. This web app uses geofencing to verify crowdsourced voting on the length of lines."
+          image="/images/lineup.jpg"
+          link="https://pure-scrubland-81366.herokuapp.com/"
+        />
+        <ProjectCard
+          title="Clash Royale RPG"
+          description="Prepare for battle, play my Clash Royale inspired role playing game!"
+          image="/images/clash-royale.jpg"
+          link="http://andrewheinke.github.io/week-4-game/"
+        />
+        <ProjectCard
+          title="NBA Trivia"
+          description="Test your NBA knowledge on my trivia challenge, fair warning it's hard!"
+          image="/images/nba-trivia.jpg"
+          link="http://andrewheinke.github.io/TriviaGame/"
+        />
+        <ProjectCard
+          title="Pokemon Hangman"
+          description="Do you want to be the very best? Test your knowledge with this hangman game."
+          image="/images/pokemon.jpg"
+          link="http://andrewheinke.github.io/week-3-game/"
+        />
+        <ProjectCard
+          title="Eat Da Burger"
+          description="A simple app to create and eat burgers. Utlizes MySQL database and Sequelize Nodejs package"
+          image="/images/hamburger.jpg"
+          link="http://immense-garden-11246.herokuapp.com/burgers"
+        />
       </Grid>
     </Container>
   );
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,79 @@
+import {
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  Text,
+  VStack,
+  Image,
+  Box,
+  Icon,
+  Spacer,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import * as React from "react";
+
+export default function ProjectCard({ image, title, description, link }) {
+  const LinkIcon = (props) => (
+    <Icon
+      viewBox="0 0 12 12"
+      w={5}
+      h={5}
+      fill="none"
+      verticalAlign="baseline"
+      {...props}
+    >
+      <path
+        d="M3.5 3a.5.5 0 000 1V3zm5 .5H9a.5.5 0 00-.5-.5v.5zm-.5 5a.5.5 0 001 0H8zm-5.354.146a.5.5 0 10.708.708l-.708-.708zM3.5 4h5V3h-5v1zM8 3.5v5h1v-5H8zm.146-.354l-5.5 5.5.708.708 5.5-5.5-.708-.708z"
+        fill="currentColor"
+      />
+    </Icon>
+  );
+
+  return (
+    <LinkBox
+      cursor="pointer"
+      position="relative"
+      rounded="md"
+      display="flex"
+      pb="3"
+      mb="4"
+      role={"group"}
+    >
+      <VStack
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"lg"}
+        rounded={"lg"}
+        pos={"relative"}
+        textAlign="left"
+        align="start"
+        zIndex={1}
+        borderWidth="1px"
+        _groupHover={{
+          bg: `${useColorModeValue("purple.100", "purple.900")}`,
+          transition: "0.2s ease-in",
+        }}
+      >
+        <Image
+          rounded={"md"}
+          height={"auto"}
+          borderBottomRightRadius="0"
+          borderBottomLeftRadius="0"
+          w="100%"
+          src={image}
+        />
+        <Box p={6}>
+          <Heading as="h3" zIndex={10} size="md" mb="2">
+            <Link href={link} passHref isExternal>
+              <LinkOverlay>
+                {title} <LinkIcon />
+              </LinkOverlay>
+            </Link>
+          </Heading>
+          <Text mb="3">{description}</Text>
+        </Box>
+      </VStack>
+    </LinkBox>
+  );
+}
